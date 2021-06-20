233deea0e76c8eb006e318f9eb8559637da5db4c
Matrosken <piranya1399@gmail.com>
2021-06-21 00:36:17 +0400

    sfg

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,13 @@
 $( document ).ready(function() {
   var swiper = new Swiper('.about_container', {
-    slidesPerView: 'auto',
-    scrollbar: {
-      el: '.swiper-scrollbar',
-      hide: false,
-      draggable: true,
-      snapOnRelease: false,
+    slidesPerView: '1',
+  });
+  var swiper1 = new Swiper('.manafacture_swiper', {
+    slidesPerView: '2',
+    speed: 1000,
+    navigation: {
+      nextEl: '.next_arrow',
+      prevEl: '.prev_arrow',
     },
   });
 });
