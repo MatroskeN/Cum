3cc0f49dd9dbbd76f9a541b4d1225abd710304dc
MatroskeN <44238649+MatroskeN@users.noreply.github.com>
2021-06-21 13:03:10 +0400

    орп

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
 $( document ).ready(function() {
   var swiper = new Swiper('.about_container', {
     slidesPerView: '1',
+    navigation: {
+      nextEl: '.next_about',
+      prevEl: '.prev_about',
+    },
   });
   var swiper1 = new Swiper('.manafacture_swiper', {
     slidesPerView: '2',
@@ -10,4 +14,36 @@ $( document ).ready(function() {
       prevEl: '.prev_arrow',
     },
   });
+  let bg = document.querySelector('.mouse-parallax-bg');
+  window.addEventListener('mousemove', function(e) {
+    let x = e.clientX / window.innerWidth;
+    let y = e.clientY / window.innerHeight;  
+    bg.style.transform = 'translate(-' + x * 30 + 'px)';
+});
+//mouse-parallax-bg-sea
+// let bg1 = document.querySelector('.mouse-parallax-bg-sea');
+//   window.addEventListener('mousemove', function(e) {
+//     let x = e.clientX / window.innerWidth;
+//     let y = e.clientY / window.innerHeight;  
+//     bg1.style.transform = 'translate(-' + x * 10 + 'px)';
+// });
+//mouse-parallax-bg-rock
+let bg2 = document.querySelector('.mouse-parallax-bg-rock');
+  window.addEventListener('mousemove', function(e) {
+    let x = e.clientX / window.innerWidth;
+    let y = e.clientY / window.innerHeight;  
+    bg2.style.transform = 'translate(-' + x * 10 + 'px)';
+});
+
+$('.lang_button').on("click", function () {
+  $('.fade_up_panel').css({'opacity':'1'});
+});
+$('.first_lang').on("click", function () {
+  $('.lang_data').html('Ru');
+  $('.fade_up_panel').css({'opacity':'0'});
+});
+$('.second_lang').on("click", function () {
+  $('.lang_data').html('Eng');
+  $('.fade_up_panel').css({'opacity':'0'});
+});
 });
